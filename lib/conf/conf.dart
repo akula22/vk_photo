7ae34037706f190ea13061635bb06843fa340b24
akula222222 <psych88@gmail.com>
2024-05-13 22:53:50 +0300

    del conf

--- lib/conf/conf.dart
+++
@@ -1,4 +0,0 @@
-const vkToken = "2b5c383a3dd64561c5b9b15c88eb63922b736b8580ae51db990c6fb1b3e1bebbfc142c70cdfbc846fb53a";
-
-/* group api url */
-const groupApiUrl = "https://akula22.tmweb.ru/public_html/api/get-groups";

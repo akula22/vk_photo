5af2949612cbd6f822a334c16e56e63f02d76e01
akula222222 <psych88@gmail.com>
2024-05-15 00:12:56 +0300

    1232132

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:vk_photo_new/repository/repository.dart';
 // import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'views/bloc_photo_view.dart';
 // import 'photo/views/photo_view.dart';
--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -3,12 +3,15 @@ import 'package:vk_photo_new/conf/conf.dart';
 import 'package:vk_photo_new/models/Group.dart';
 
 class Repository {
-  static fetchGroups() async {
+Future fetchGroups() async {
     var dio = Dio();
     try {
       final response = await dio.get(groupApiUrl);
       if (response.statusCode == 200) {
         return (response.data as List).map((e) => Group.fromJson(e));
+      }
+      else {
+        return [];
       }
     } catch (e) {
       print(e.toString());
